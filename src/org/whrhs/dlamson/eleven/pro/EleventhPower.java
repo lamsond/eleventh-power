bf471fbf1b4aaddd6a67dd062d94798292b5fe07
dlamson <dlamson@LTP-DLamson.whrhs.org>
2019-11-11 20:28:56 -0500

    testing git perspective

--- src/org/whrhs/dlamson/eleven/pro/EleventhPower.java
+++ src/org/whrhs/dlamson/eleven/pro/EleventhPower.java
@@ -2,6 +2,6 @@ package org.whrhs.dlamson.eleven.pro;
 
 public class EleventhPower {
 	public static void main(String[] args) {
-		System.out.println("you down wit OCP?");
+		System.out.println("you down wit OCP???");
 	}
 }
